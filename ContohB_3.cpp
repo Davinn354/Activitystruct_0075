a26b491da47bb8141fcc2464bac8f97b62248fd7
Davinn354 <daviipad53@gmail.com>
2024-04-01 15:44:41 +0700

    Penambahan hasil data

--- ContohB_3.cpp
+++ ContohB_3.cpp
@@ -15,10 +15,23 @@ struct Mahasiswa{
 int main(){
     Mahasiswa mhs;
     cout << "Nomor Mahasiswa : ";
-    getline(cin, mhs.nim);
+    getline (cin, mhs.nim);
     cout << "Nama Mahasiswa : ";
-    getline(cin, mhs.nama);
+    getline (cin, mhs.nama);
 
-    
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa : ";
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : ";
+    cout << "\n \t Desa : " << mhs.alamat.desa;
+    cout << "\n \t Kota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
 }
 
